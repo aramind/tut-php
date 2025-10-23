02b151ba5c5a21acb9543a80bd43c76c96b91e0e
aramind <88814622+aramind@users.noreply.github.com>
2025-10-24 02:06:58 +0800

    feat: separating logic and view

--- yt-laracasts/index.php
+++ yt-laracasts/index.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <style>
-      body {
-        display: grid;
-        place-items: center;
-        height: 100vh;
-        margin: 0;
-        font-family: sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Recommended Books</h1>
-
-    <?php $books = [
+    <?php 
+    
+    $books = [
       [
         "name" => "Do Androids Dream of Electric Sheep",
         "author" => "Philip K. Dick",
@@ -50,19 +33,6 @@
       return $filteredBooks;
     }  
 
-    ?>
 
+require 'index.view.php';
     
-    <ul>
-      <?php forEach (filterByAuthor($books, "Andy Weir") as $book) : ?>
-       
-          <li>
-          <a href="<?= $book["purchaseUrl"] ?>">
-            <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book["author"] ?>
-          </a>
-          </li>
-       
-        <?php endforeach; ?>
-    </ul>
-  </body>
-</html>
