1abcff4609e26031f4a49389510014e3198f1dbf
aramind <88814622+aramind@users.noreply.github.com>
2025-10-28 19:24:06 +0800

    feat: display a note in a separate page

--- yt-laracasts/router.php
+++ yt-laracasts/router.php
@@ -7,6 +7,7 @@ $routes = [
     '/tut-php/yt-laracasts/about' => 'controllers/about.php',
     '/tut-php/yt-laracasts/contact' => 'controllers/contact.php',
     '/tut-php/yt-laracasts/notes' => 'controllers/notes.php',
+    '/tut-php/yt-laracasts/note' => 'controllers/note.php',
 
 ];
 
--- yt-laracasts/views/notes.view.php
+++ yt-laracasts/views/notes.view.php
@@ -3,8 +3,13 @@
 <?php require("partials/banner.php") ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
-        <p>Now you are on the notes page</p>
+        <?php foreach ($notes as $note) : ?>
+            <li>
+                <a href="/tut-php/yt-laracasts/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline hover:text-blue-300">
+                    <?= $note['body'] ?>
+                </a>
+            </li>
+        <?php endforeach ?>
     </div>
 </main>
 <?php require("partials/footer.php") ?>
