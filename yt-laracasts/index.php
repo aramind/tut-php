f8333b8bf419b4a1779bac62250a918fa4124987
aramind <88814622+aramind@users.noreply.github.com>
2025-10-28 12:14:54 +0800

    extracting the db class

--- yt-laracasts/index.php
+++ yt-laracasts/index.php
@@ -1,34 +1,17 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 // require 'router.php';
 
 // connect to our mySQL database
 
-class Database
-{
-    public $connection;
 
-    public function __construct()
-    {
-        $dsn = 'mysql:host=localhost;port=3306;dbname=yt_laracasts;charset=utf8mb4';
-
-        $this->connection = new PDO($dsn, 'root');
-    }
-    public function query($query)
-    {
-
-
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement;
-    }
-}
 
 $db = new Database();
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
+// dd($posts);
 foreach ($posts as $post) {
     echo "<li> {$post['title']} </li>";
 }
