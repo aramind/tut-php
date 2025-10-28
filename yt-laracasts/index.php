35445a0cc00fd4f6e96468b4606d571fd3c08174
aramind <88814622+aramind@users.noreply.github.com>
2025-10-28 11:24:19 +0800

    feat: initial code for connecting to db

--- yt-laracasts/index.php
+++ yt-laracasts/index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to our mySQL database
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=yt_laracasts;charset=utf8mb4';
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
