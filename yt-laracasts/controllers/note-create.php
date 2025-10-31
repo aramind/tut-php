2861ce4563021cd4dfe63344cbb1bf9b442ceca1
aramind <88814622+aramind@users.noreply.github.com>
2025-10-31 18:38:14 +0800

    feat: add max char validation

--- yt-laracasts/controllers/note-create.php
+++ yt-laracasts/controllers/note-create.php
@@ -12,6 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $errors['body'] = "A body is required";
     }
 
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = "The body cannot be more than 1,000 characters";
+    }
+
     if (empty($errors)) {
         $db->query('INSERT INTO notes (body, user_id VALUES (:body, :user_id)', ['body' => $_POST['body'], 'user_id' => 1]);
     }
