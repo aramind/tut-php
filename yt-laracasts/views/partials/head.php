ae21efb3329bd74dbce71b525f0d70e298c73441
aramind <88814622+aramind@users.noreply.github.com>
2025-10-31 15:21:29 +0800

    feat: add ui for create note page

--- yt-laracasts/views/partials/head.php
+++ yt-laracasts/views/partials/head.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-900">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <!-- ✅ Load TailwindCSS first -->
-     
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> 
-    <style>
-  :root {
-    color-scheme: light;
-  }
-  html {
-    background-color: white !important;
-    color: black !important;
-  }
-</style>
-    
-  </head>
-  <body class="h-full">
-<div class="min-h-full">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <!-- ✅ Load TailwindCSS first -->
+
+  <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+  <style>
+    :root {
+      color-scheme: light;
+    }
+
+    html {
+      background-color: white !important;
+      color: black !important;
+    }
+  </style>
+
+</head>
+
+<body class="h-full">
+  <div class="min-h-full">
