b2c0165213835c0e204be48b37dbabd3f6e6fbaf
aramind <88814622+aramind@users.noreply.github.com>
2025-11-01 11:47:19 +0800

    feat: making the validator static function

--- yt-laracasts/controllers/note-create.php
+++ yt-laracasts/controllers/note-create.php
@@ -2,22 +2,20 @@
 
 $config = require('config.php');
 $db = new Database($config['database']);
+require "Validator.php";
 
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     $errors = [];
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = "A body is required";
-    }
 
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = "The body cannot be more than 1,000 characters";
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
 
     if (empty($errors)) {
-        $db->query('INSERT INTO notes (body, user_id VALUES (:body, :user_id)', ['body' => $_POST['body'], 'user_id' => 1]);
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', ['body' => $_POST['body'], 'user_id' => 1]);
     }
 }
 
