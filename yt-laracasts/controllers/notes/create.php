2cb0c1c3b817a0411070fb1cf309a5a805631c1e
aramind <88814622+aramind@users.noreply.github.com>
2025-11-01 12:44:48 +0800

    refactor(marjor): file and import organization

--- yt-laracasts/controllers/notes/create.php
+++ yt-laracasts/controllers/notes/create.php
@@ -1,15 +1,14 @@
 <?php
 
-$config = require('config.php');
+$config = require base_path('config.php');
 $db = new Database($config['database']);
-require "Validator.php";
+require base_path("Validator.php");
 
-$heading = "Create Note";
-
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-    $errors = [];
+
 
     if (!Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = 'A body of no more than 1,000 characters is required';
@@ -20,5 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     }
 }
 
-
-require "views/notes/create.view.php";
+view("notes/create.view.php", [
+    'heading' => "Create Note",
+    'errors' => $errors,
+]);
