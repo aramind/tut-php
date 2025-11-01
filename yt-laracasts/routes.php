a9a102a2af90434c400da47ccb30ff6b526f2e9e
aramind <88814622+aramind@users.noreply.github.com>
2025-11-01 12:10:21 +0800

    refactor: file organization

--- yt-laracasts/routes.php
+++ yt-laracasts/routes.php
@@ -4,8 +4,8 @@ return [
     '/tut-php/yt-laracasts/' => 'controllers/index.php',
     '/tut-php/yt-laracasts/about' => 'controllers/about.php',
     '/tut-php/yt-laracasts/contact' => 'controllers/contact.php',
-    '/tut-php/yt-laracasts/notes' => 'controllers/notes.php',
-    '/tut-php/yt-laracasts/note' => 'controllers/note.php',
-    '/tut-php/yt-laracasts/notes/create' => 'controllers/note-create.php',
+    '/tut-php/yt-laracasts/notes' => 'controllers/notes/index.php',
+    '/tut-php/yt-laracasts/note' => 'controllers/notes/show.php',
+    '/tut-php/yt-laracasts/notes/create' => 'controllers/notes/create.php',
 
 ];
