f0ab7ee4980890037f5aaf5573922a400a4c9ffb
aramind <88814622+aramind@users.noreply.github.com>
2025-11-03 17:14:23 +0800

    fix: delete a note

--- yt-laracasts/routes.php
+++ yt-laracasts/routes.php
@@ -3,7 +3,10 @@
 $router->get("/tut-php/yt-laracasts/", 'controllers/index.php');
 $router->get("/tut-php/yt-laracasts/about", 'controllers/about.php');
 $router->get("/tut-php/yt-laracasts/contact", 'controllers/contact.php');
+
+// notes
 $router->get("/tut-php/yt-laracasts/notes", 'controllers/notes/index.php');
 $router->get("/tut-php/yt-laracasts/note", 'controllers/notes/show.php');
+$router->delete("/tut-php/yt-laracasts/note", 'controllers/notes/destroy.php');
 $router->get("/tut-php/yt-laracasts/notes/create", 'controllers/notes/create.php');
 $router->post("/tut-php/yt-laracasts/notes/create", 'controllers/notes/create.php');
