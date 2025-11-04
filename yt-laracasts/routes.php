39361f60aff23f127334a58fde9667c4d736dfdf
aramind <88814622+aramind@users.noreply.github.com>
2025-11-04 13:25:46 +0800

    feat: add edit page

--- yt-laracasts/routes.php
+++ yt-laracasts/routes.php
@@ -8,5 +8,8 @@ $router->get("/tut-php/yt-laracasts/contact", 'controllers/contact.php');
 $router->get("/tut-php/yt-laracasts/notes", 'controllers/notes/index.php');
 $router->get("/tut-php/yt-laracasts/note", 'controllers/notes/show.php');
 $router->delete("/tut-php/yt-laracasts/note", 'controllers/notes/destroy.php');
+
+$router->get("/tut-php/yt-laracasts/note/edit", 'controllers/notes/edit.php');
+
 $router->get("/tut-php/yt-laracasts/notes/create", 'controllers/notes/create.php');
 $router->post("/tut-php/yt-laracasts/notes", 'controllers/notes/store.php');
