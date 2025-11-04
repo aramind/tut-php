243e78e50bbbfe9a112d5236fd9230278df1478c
aramind <88814622+aramind@users.noreply.github.com>
2025-11-04 13:21:24 +0800

    feat: create container for injecting the database instance

--- yt-laracasts/public/index.php
+++ yt-laracasts/public/index.php
@@ -10,6 +10,9 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
+# ✅ Load container bindings here
+require base_path('bootstrap.php');
+
 $router = new \Core\Router();
 
 $routes = require base_path('routes.php');
