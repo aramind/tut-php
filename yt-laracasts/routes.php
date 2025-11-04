7d9033503d9dc29621d32a74981c20b2fd853b28
aramind <88814622+aramind@users.noreply.github.com>
2025-11-04 15:23:51 +0800

    feat; adding ui for register account

--- yt-laracasts/routes.php
+++ yt-laracasts/routes.php
@@ -14,3 +14,6 @@ $router->patch("/tut-php/yt-laracasts/note", 'controllers/notes/update.php');
 
 $router->get("/tut-php/yt-laracasts/notes/create", 'controllers/notes/create.php');
 $router->post("/tut-php/yt-laracasts/notes", 'controllers/notes/store.php');
+
+
+$router->get('/tut-php/yt-laracasts/register', 'controllers/registration/create.php');
